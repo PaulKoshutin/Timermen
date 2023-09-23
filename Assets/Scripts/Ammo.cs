e8366a90a7ab9aebfdf5be6e7b062b34d1ef55b7
PaulKoshutin <koshutin.pavel@yandex.ru>
2023-09-24 00:33:38 +0300

    Stage 3

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    public float damage;
+    public float initialDistance;
+    public float effectiveDistance;
+    public Vector3 target;
+
+    private void Start()
+    {
+        effectiveDistance = initialDistance;
+    }
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, 20 * Time.deltaTime);
+        if (transform.position == target)
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -9,14 +9,17 @@ public class Ship : MonoBehaviour
     public float scanRange;
     public float scanSize;
     public List<GameObject> scannedFoes;
-    private float attack;
-    private float defense;
-    private float healthPoints;
+    public List<string> knownFoes;
+    public float attack;
+    public float healthPoints;
     public bool isMoving = false;
     public Vector3 targetPosition;
     private float speed = 3.0f;
+    public float maxRechargeTime;
+    public float rechargeTime = 0;
 
     public GameObject indicatorPrefab;
+    public GameObject ammoPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +33,7 @@ public class Ship : MonoBehaviour
         {
             Move();
         }
+        OpenFire();
     }
 
     void OnDestroy()
@@ -50,6 +54,8 @@ public class Ship : MonoBehaviour
     {
         Scan();
         CursorTrail();
+        if (rechargeTime > 0)
+            rechargeTime -= 1;
     }
 
     void Move()
@@ -70,6 +76,16 @@ public class Ship : MonoBehaviour
         else
             tagToFind = "AI";
 
+        for (int i=0;i<scannedFoes.Count;i++)
+            if (scannedFoes[i] == null)
+            {
+                string name = knownFoes[i];
+                scannedFoes.RemoveAt(i);
+                Destroy(transform.Find("Indicator" + name).gameObject);
+                knownFoes.RemoveAt(i);
+                break;
+            }
+
         GameObject[] foes = GameObject.FindGameObjectsWithTag(tagToFind);
         foreach (GameObject foe in foes)
         {
@@ -87,17 +103,19 @@ public class Ship : MonoBehaviour
                         if (width < 0.05)
                             width = 0.05f;
                         indicator.GetComponentInChildren<SpriteRenderer>().size = new Vector2(width, 1f);
-                        Vector3 dir = foe.transform.position - transform.position;
+                        Vector2 dir = foe.transform.position - transform.position;
                         dir = foe.transform.InverseTransformDirection(dir);
                         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+                        angle += foe.transform.eulerAngles.z;
                         float randomError = Random.Range(-width * 5, width * 5);
                         angle += randomError;
                         indicator.eulerAngles = new Vector3(0, 0, angle);
-                        StartCoroutine(Rotate(angle, indicator.transform));
+                        indicator.transform.eulerAngles = new Vector3(0, 0, angle);
                     }
                     else
                     {
                         scannedFoes.Add(foe);
+                        knownFoes.Add(foe.name);
                         GameObject indicator = Instantiate(indicatorPrefab, transform) as GameObject;
                         float width = 2 / result;
                         if (width < 0.05)
@@ -105,13 +123,13 @@ public class Ship : MonoBehaviour
                         indicator.GetComponentInChildren<SpriteRenderer>().size = new Vector2(width, 1f);
                         indicator.transform.parent = transform;
                         indicator.name = "Indicator" + foe.name;
-                        Vector3 dir = foe.transform.position - transform.position;
+                        Vector2 dir = foe.transform.position - transform.position;
                         dir = foe.transform.InverseTransformDirection(dir);
                         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+                        angle += foe.transform.eulerAngles.z;
                         float randomError = Random.Range(-width * 5, width * 5);
                         angle += randomError;
-                        //indicator.transform.eulerAngles = new Vector3(0, 0, angle);
-                        StartCoroutine(Rotate(angle, indicator.transform));
+                        indicator.transform.eulerAngles = new Vector3(0, 0, angle);
                     }
                 }
                 else
@@ -129,14 +147,14 @@ public class Ship : MonoBehaviour
     {
         if (CompareTag("Player"))
         {
-            StopAllCoroutines();
+            //StopAllCoroutines();
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Transform gun = transform.Find("GunSlot");
             Vector3 dir = mousePosition - transform.position;
             dir = transform.InverseTransformDirection(dir);
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
-            //gun.eulerAngles = new Vector3(0, 0, angle);
-            StartCoroutine(Rotate(angle, gun));
+            gun.eulerAngles = new Vector3(0, 0, angle);
+            //StartCoroutine(Rotate(angle, gun));
         }
     }
     IEnumerator Rotate(float targetAngle, Transform obj)
@@ -149,23 +167,43 @@ public class Ship : MonoBehaviour
         obj.rotation = Quaternion.Euler(0f, 0f, targetAngle);
         yield return null;
     }
-    void attackShip(Ship ship)
+    void OpenFire()
     {
-
+        if (CompareTag("Player"))
+        {
+            if (Input.GetMouseButtonDown(1) && rechargeTime == 0)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Transform gun = transform.Find("GunSlot");
+                float distance = Vector3.Distance(transform.position, mousePosition);
+                GameObject ammo = Instantiate(ammoPrefab) as GameObject;
+                ammo.transform.eulerAngles = new Vector3(0, 0, gun.eulerAngles.z);
+                ammo.transform.position = transform.position;
+                ammo.GetComponent<Ammo>().damage = attack;
+                ammo.GetComponent<Ammo>().initialDistance = distance;
+                ammo.GetComponent<Ammo>().target = mousePosition;
+                rechargeTime = maxRechargeTime;
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ammo")
+        {
+            getDamage(collision.gameObject.GetComponent<Ammo>().damage);
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "AI" || collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 
     void getDamage(float damage)
     {
-        if ((damage - defense) <= 0)
-        {
-            damage = 0;
-        }
-        else
-        {
-            damage -= defense;
-        }
-
         this.healthPoints -= damage;
-
+        if (healthPoints <= 0)
+            Destroy(gameObject);
     }
 }
